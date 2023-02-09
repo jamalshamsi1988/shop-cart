1eb7d386de0e87492d73d2317b4bf536fe29d6c3
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-02-09 14:31:10 -0800

    add linkedin &github icon

--- cart-shop/src/components/AboutUs.js
+++ cart-shop/src/components/AboutUs.js
@@ -4,6 +4,8 @@ import React from 'react';
 import myFoto from "../image/Jamal_Shamsi.jpg";
 //CSS
 import styles from "../components/AboutUs.module.css";
+//Icons
+import github from "../assets/Icons/github.svg"
 
 
 const AboutUs = () => {
@@ -16,7 +18,8 @@ const AboutUs = () => {
      
      <h3>Interested in Web development, especially enjoys working with teams.
         wants to learn new things about Web and it's around. Always have an energetic personality in work.
-        </h3>
+        </h3>  
+<img className={styles.icon} src={github} />
 </div>
     </div>
   )
